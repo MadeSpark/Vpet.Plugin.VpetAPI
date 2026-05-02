53810d9811d8ad8bddf31880708723862cf0af70
Madespark <3553142133@qq.com>
2026-05-02 18:37:05 +0800

    新增对LinePutScript.Localization.WPF的引用

--- VpetStateController.cs
+++ VpetStateController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using LinePutScript.Localization.WPF;
 using VPet_Simulator.Windows.Interface;
 
 namespace VPet.Plugin.VpetAPI
